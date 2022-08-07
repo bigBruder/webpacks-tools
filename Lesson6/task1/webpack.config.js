d68ae9540c9383b5a3053c76f3b322501eb06b49
romontherock <romontherock@example.com>
2022-08-07 09:09:51 +0300

    vfskn

--- Lesson6/task1/webpack.config.js
+++ Lesson6/task1/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     rules: [
       {
         test: /.s?css$/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /.(jpg|png)$/,
