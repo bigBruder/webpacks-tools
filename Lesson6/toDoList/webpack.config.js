20ee50b039dee624cb93c29cd4fbc9bc977e4912
romontherock <romontherock@example.com>
2022-08-07 23:24:53 +0300

    vfskn

--- Lesson6/toDoList/webpack.config.js
+++ Lesson6/toDoList/webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { webpack } = require("webpack");
+const webpack = require("webpack");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
+
   const config = {
     entry: "./src/index.js",
     output: {
@@ -51,7 +52,6 @@ module.exports = (env, argv) => {
       hot: true,
     },
   };
-
   if (isProduction) {
     config.plugins.push(
       new MiniCssExtractPlugin({
